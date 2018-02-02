2f52555a4738e29d74593b61d44dc397b990f6b4
David Fraley <fraleyds@gmail.com>
2018-02-02 10:32:47 -0500

    particles-js added

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// var canvas = document.querySelector('canvas');
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight * 0.5;
+
+// var c = canvas.getContext('2d');
+
+// var mouse = {
+//     x: undefined,
+//     y: undefined
+// }
+
+// window.addEventListener('mousemove',
+//     function(event) {
+//         mouse.x = event.x;
+//         mouse.y = event.y;
+//     }
+// )
+
+// window.addEventListener('resize',
+//     function() {
+//         canvas.width = window.innerWidth;
+//         canvas.height = window.innerHeight * 0.5;
+//     }
+// )
